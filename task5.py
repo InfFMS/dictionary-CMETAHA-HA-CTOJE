02a0685fcf5c989c9bc15c6d35715a7beefda1f1
CMETAHA-HA-CTOJE <yaroslav.alelekov@gmail.com>
2025-05-05 14:38:02 +0700

    Пятое доделано

--- task5.py
+++ task5.py
@@ -6,12 +6,34 @@
 # Используйте словари для словаря:)
 engWords = 'animal, apple, arm, baby, badminton, bag, ball, balloon, banana, bear, bed, bee, bike, bird, blackboard, block, book, box'
 engWords = engWords.split(', ')
+
 rusWords = 'животное яблоко рука малыш бадминтон сумка мяч воздушный шар банан медведь кровать пчела велосипед птица доска кубик книга коробка'
 rusWords = rusWords.split()
+
 eng_rusWords = dict(zip(engWords, rusWords))
-word = input('eng to rus >> ')
-outWord = eng_rusWords.get(word)
-if outWord:
-    print(outWord)
-else:
-    print('translation is not available')
+rus_engWords = dict(zip(rusWords, engWords))
+
+
+def eng_rus(word):
+    outRusWord = eng_rusWords.get(word)
+    if outRusWord:
+        return outRusWord
+    else:
+        return 'translation is not available'
+
+
+def rus_eng(word):
+    outEngWord = rus_engWords.get(word)
+    if outEngWord:
+        return outEngWord
+    else:
+        return 'перевод не имеется'
+
+
+fl = input('eng to rus (write eng), руское в английское? (напишите рус) >> ')
+if fl == 'eng':
+    word = input('word >> ')
+    print(eng_rus(word))
+if fl == 'рус':
+    word = input('слово >> ')
+    print(rus_eng(word))
