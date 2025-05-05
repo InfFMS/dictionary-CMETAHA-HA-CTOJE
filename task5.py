5a89871f43298d368da75168e02115ccb3c5bf16
CMETAHA-HA-CTOJE <yaroslav.alelekov@gmail.com>
2025-05-05 14:22:14 +0700

    Первые пять

--- task5.py
+++ task5.py
@@ -4,3 +4,14 @@
 # и предлагает пользователю ввести слово для перевода. 
 # Если введенного слова нет в словаре выводится сообщение "нет такого слова".
 # Используйте словари для словаря:)
+engWords = 'animal, apple, arm, baby, badminton, bag, ball, balloon, banana, bear, bed, bee, bike, bird, blackboard, block, book, box'
+engWords = engWords.split(', ')
+rusWords = 'животное яблоко рука малыш бадминтон сумка мяч воздушный шар банан медведь кровать пчела велосипед птица доска кубик книга коробка'
+rusWords = rusWords.split()
+eng_rusWords = dict(zip(engWords, rusWords))
+word = input('eng to rus >> ')
+outWord = eng_rusWords.get(word)
+if outWord:
+    print(outWord)
+else:
+    print('translation is not available')
