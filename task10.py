185a97673544682fad3adf7760109ae5608b507a
CMETAHA-HA-CTOJE <yaroslav.alelekov@gmail.com>
2025-05-12 13:14:56 +0700

    Десятое

--- task10.py
+++ task10.py
@@ -4,3 +4,17 @@
 # потом после пробела - значение (может быть строкой, числом, или набором чисел),
 # все, что после символа "//" это комментарий  должно игнорироваться.
 # Реализуйте считывание значений из файла и запись этих значений в словарь.
+variables = {}
+with open('parameters.txt', encoding='utf-8') as file:
+    for line in file:
+        variable = str()
+        value = str()
+        lineSplit = line.split()
+        for i in range(len(lineSplit)):
+            if lineSplit[i][0] == '/' and lineSplit[i][1] == '/':
+                lineSplit = lineSplit[:i]
+                break
+        if len(lineSplit) != 0:
+            variables.update({f'{lineSplit[0]}':f'{lineSplit[1]}'})
+print(variables)
+
